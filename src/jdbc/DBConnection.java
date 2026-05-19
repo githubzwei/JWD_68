ba6e10ee833ca5f3b94a098f3b59ac1410117658
githubzwei <mayzwei@gmail.com>
2026-05-19 16:27:04 +0630

    add print method

--- src/jdbc/DBConnection.java
+++ src/jdbc/DBConnection.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 public static void main(String[] args) {
+	System.out.println("update DBConnection!!!");
 	System.out.println("DBConnection Test");
 	System.out.println("update DBConnection Test!!!");
 	
