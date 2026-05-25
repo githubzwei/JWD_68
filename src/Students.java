034a7287768cf640a23c9321986e81ac8c06c4b4
githubzwei <mayzwei@gmail.com>
2026-05-25 15:32:01 +0630

    mz

--- src/Students.java
+++ src/Students.java
@@ -5,10 +5,10 @@ public class Students {
 
 	private String name;
 	private int age;
-<<<<<<< HEAD
+
 	private double mark;
 	private int petname;
-=======
+
 	private String password;
 	
 	public String getPassword() {
@@ -29,7 +29,6 @@ public class Students {
 	public void setAge(int age) {
 		this.age = age;
 	}
->>>>>>> 34e4d323e21d6828914fd8bb1255768483cd1bed
 	
 	
 }
