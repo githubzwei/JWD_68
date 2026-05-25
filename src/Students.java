34e4d323e21d6828914fd8bb1255768483cd1bed
KhineKhinWin <khinekhinwinkhinekhinwin@gmail.com>
2026-05-25 15:24:01 +0630

    Update Students.java

--- src/Students.java
+++ src/Students.java
@@ -1,15 +1,30 @@
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
-@ToString
+
+
 public class Students {
 
 	private String name;
 	private int age;
-	private double mark;
+	private String password;
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 	
 	
 }
